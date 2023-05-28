eff4f33c32e76cbc5f51179ccfa4438c6c98ac63
JechtSh0t <72933425+JechtSh0t@users.noreply.github.com>
2023-05-28 18:37:19 -0400

    [Feature] Add nib initializer (#1)

--- Sources/ImageLoaderView.swift
+++ Sources/ImageLoaderView.swift
@@ -28,7 +28,9 @@ public class ImageLoaderView: UIImageView {
 	}
 	
 	required init?(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
+		
+		super.init(coder: coder)
+		ImageLoader.addObserver(self, selector: #selector(handleNotification))
 	}
 }
 
